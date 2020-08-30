37836681c04014704971b02390504503efb40b89
hogucc <uku.h1r8@gmail.com>
2020-08-30 15:48:38 +0900

    resistor_color_duo_リファクタリング後

--- excersise/excersism/resistor_color_duo/resistor_color_duo.rb
+++ excersise/excersism/resistor_color_duo/resistor_color_duo.rb
@@ -1,19 +1,7 @@
 class ResistorColorDuo
-  COLOR_MAPPING = {
-    "black" => 0,
-    "brown" => 1,
-    "red" => 2,
-    "orange" => 3,
-    "yellow" => 4,
-    "green" => 5,
-    "blue" => 6,
-    "violet" => 7,
-    "grey" => 8,
-    "white" => 9
-  }.freeze
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.value(colors)
-    colors = colors.shift(2)
-    colors.map { |color| COLOR_MAPPING[color] }.join.to_i
+    colors.take(2).map { |color| COLORS.index(color) }.join.to_i
   end
 end
