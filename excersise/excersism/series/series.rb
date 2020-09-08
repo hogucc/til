9d1f1f55a0d85c15d6a3389b3f08231838192a3c
hogucc <uku.h1r8@gmail.com>
2020-09-08 23:36:35 +0900

    each_consを使用して修正

--- excersise/excersism/series/series.rb
+++ excersise/excersism/series/series.rb
@@ -5,11 +5,6 @@ class Series
 
   def slices(num)
     raise ArgumentError if @str.length < num
-    slice_str = @str.each_with_index.map do |c, idx|
-      @str[idx...(idx + num)].join
-    end
-
-    slice_str.delete_if {|str| str.length < num}
+    @str.each_cons(num).map(&:join)
   end
 end
-
