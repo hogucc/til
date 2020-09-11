275b5519db55f00df799d495b625e0b55ac54f13
hogucc <uku.h1r8@gmail.com>
2020-09-11 22:18:46 +0900

    hamming_リファクタリング後

--- excersise/excersism/hamming/hamming.rb
+++ excersise/excersism/hamming/hamming.rb
@@ -1,18 +1,6 @@
 class Hamming
   def self.compute(text_a, text_b)
     raise ArgumentError if text_a.size != text_b.size
-
-    text_size = text_a.size
-    diff_num = 0
-
-    if text_a == text_b
-      0
-    else
-      (0...text_size).each do |size|
-        diff_num += 1 if text_a[size] != text_b[size]
-      end
-
-      diff_num
-    end
+    text_a.chars.zip(text_b.chars).count{ |a,b| a != b }
   end
 end
