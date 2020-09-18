aabb696cef1c4fd612eb2bcaffe3775d4ad3108c
hogucc <uku.h1r8@gmail.com>
2020-09-18 23:41:43 +0900

    ハッシュを定数からクラスインスタンス変数に変更

--- excersise/excersism/scrabble_score/scrabble_score.rb
+++ excersise/excersism/scrabble_score/scrabble_score.rb
@@ -1,11 +1,16 @@
 class Scrabble
-  LETTERS = {
-    "A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1,
-    "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1,
-    "D" => 2, "G" => 2, "B" => 3, "C" => 3, "M" => 3,
-    "P" => 3, "F" => 4, "H" => 4, "V" => 4, "W" => 4,
-    "Y" => 4, "K" => 5, "J" => 8, "X" => 8, "Q" => 10,
-    "Z" => 10 }
+  score_to_letter = {
+    1  => [ 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' ],
+    2  => [ 'D', 'G' ],
+    3  => [ 'B', 'C', 'M', 'P' ],
+    4  => [ 'F', 'H', 'V', 'W', 'Y' ],
+    5  => [ 'K' ],
+    8  => [ 'J', 'X' ],
+    10 => [ 'Q', 'Z' ]
+  }
+  
+  letter_to_score = Hash.new
+  score_to_letter.each_pair { |k,v| v.each { |x| letter_to_score[x] = k } }
 
   def initialize(str)
     @str = str.to_s.upcase
@@ -13,7 +18,7 @@ class Scrabble
   
   def score
     score = 0
-    LETTERS.each do |key, value|
+    score_to_letter.each do |key, value|
       score += (@str.scan(key).count * value)
     end
     score
