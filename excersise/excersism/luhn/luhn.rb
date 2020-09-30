2ccd7f61e48ce09e60225e9e59f728feb9d03b57
hogucc <uku.h1r8@gmail.com>
2020-09-30 23:40:19 +0900

    luhnアルゴリズム

--- excersise/excersism/luhn/luhn.rb
+++ excersise/excersism/luhn/luhn.rb
@@ -1,20 +1,15 @@
 class Luhn
   def self.valid?(str)
-    return false if str.length <= 1
-
     array = []
-    str.strip.split(" ").map do |str|
-      str.reverse.each_char.with_index.map do |c, index|
-        # TODO 数字以外ならfalseにする
-        c = c.to_i
-        c = c * 2 if (index + 1) % 2 == 0
-        c = c - 9 if c > 9
-        p c
-        array << c
-      end
+    str = str.strip.delete(" ").reverse
+    return false if str.length <= 1
+    return false unless /\A[0-9]+\z/.match?(str)
+    str.each_char.with_index.map do |c, index|
+      c = c.to_i
+      c = c * 2 if (index + 1) % 2 == 0
+      c = c - 9 if c > 9
+      array << c
     end
-
-    p array.sum
 
     return false if array.sum % 10 != 0
 
--- excersise/excersism/luhn/luhn_test.rb
+++ excersise/excersism/luhn/luhn_test.rb
@@ -4,27 +4,27 @@ require_relative 'luhn'
 # Common test data version: 1.4.0 4a80663
 class LuhnTest < Minitest::Test
   def test_single_digit_strings_can_not_be_valid
-    skip
+    # skip
     refute Luhn.valid?("1")
   end
 
   def test_a_single_zero_is_invalid
-    skip
+    # skip
     refute Luhn.valid?("0")
   end
 
   def test_a_simple_valid_sin_that_remains_valid_if_reversed
-    skip
+    # skip
     assert Luhn.valid?("059")
   end
 
   def test_a_simple_valid_sin_that_becomes_invalid_if_reversed
-    skip
+    # skip
     assert Luhn.valid?("59")
   end
 
   def test_a_valid_canadian_sin
-    skip
+    # skip
     assert Luhn.valid?("055 444 285")
   end
 
@@ -49,37 +49,37 @@ class LuhnTest < Minitest::Test
   end
 
   def test_valid_strings_with_a_non_digit_added_at_the_end_become_invalid
-    skip
+    # skip
     refute Luhn.valid?("059a")
   end
 
   def test_valid_strings_with_punctuation_included_become_invalid
-    skip
+    # skip
     refute Luhn.valid?("055-444-285")
   end
 
   def test_valid_strings_with_symbols_included_become_invalid
-    skip
+    # skip
     refute Luhn.valid?("055£ 444$ 285")
   end
 
   def test_single_zero_with_space_is_invalid
-    skip
+    # skip
     refute Luhn.valid?(" 0")
   end
 
   def test_more_than_a_single_zero_is_valid
-    skip
+    # skip
     assert Luhn.valid?("0000 0")
   end
 
   def test_input_digit_9_is_correctly_converted_to_output_digit_9
-    skip
+    # skip
     assert Luhn.valid?("091")
   end
 
   def test_strings_with_non_digits_is_invalid
-    skip
+    # skip
     refute Luhn.valid?(":9")
   end
 end
