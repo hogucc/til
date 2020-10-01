85aec49843e28df4d28dafa160e8c5d772fbbce4
hogucc <uku.h1r8@gmail.com>
2020-10-01 23:40:26 +0900

    メソッド抽出前

--- ruby/books/refactoring_ruby_edition/extract_method.rb
+++ ruby/books/refactoring_ruby_edition/extract_method.rb
@@ -8,3 +8,26 @@ def print_details(amount)
   puts "name: #{@name}"
   puts "amount: #{amount}"
 end
+
+# メソッド抽出前
+def print_owing(amount)
+  outstanding = 0.0
+
+  print_banner
+  
+  # 勘定を計算
+  @orders.each do |order|
+    outstanding += order.amount
+  end
+
+  # 詳細を表示
+  puts "name: #{@name}"
+  puts "amount: #{amount}" 
+end
+
+def print_banner
+  # バナーを出力
+  puts "**************************"
+  puts "*****Customer Owes********"
+  puts "**************************"
+end
