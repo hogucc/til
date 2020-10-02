ee0ab52a1c7cda2102bf32ad3a08cfd8c37edd06
hogucc <uku.h1r8@gmail.com>
2020-10-02 23:12:44 +0900

    ローカル変数を後続の使用する箇所のメソッド抽出

--- ruby/books/refactoring_ruby_edition/extract_method.rb
+++ ruby/books/refactoring_ruby_edition/extract_method.rb
@@ -1,26 +1,22 @@
-# メソッド抽出前
 def print_owing(amount)
-  outstanding = 0.0
-
   print_banner
-  
-  # 勘定を計算
-  @orders.each do |order|
-    outstanding += order.amount
-  end
-
+  # メソッドで使用しているローカル変数を後続の処理でも使う場合は戻り値にして一時変数に代入する
+  outstanding = calculate_outstanding
   print_details(outstanding)
 end
 
 def print_banner
-  # バナーを出力
   puts "**************************"
   puts "*****Customer Owes********"
   puts "**************************"
 end
 
-# ローカル変数を使用している箇所をメソッド抽出する
 def print_details(outstanding)
   puts "name: #{@name}"
   puts "amount: #{outstanding}"
 end
+
+def calculate_outstanding
+  @orders.inject(0.0) { |result, order| result + order.amount }
+end
+ 
