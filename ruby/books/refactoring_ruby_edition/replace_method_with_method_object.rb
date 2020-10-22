3b5d5947b3c7c5d3510fc9b71c4daf49a9f1ee05
hogucc <uku.h1r8@gmail.com>
2020-10-23 00:12:18 +0900

    メソッドの抽出

--- ruby/books/refactoring_ruby_edition/replace_method_with_method_object.rb
+++ ruby/books/refactoring_ruby_edition/replace_method_with_method_object.rb
@@ -11,6 +11,7 @@ class Account
   end
 end
 
+# ----------------------------------------
 # リファクタリングの過程
 # クラスの宣言
 class Gammma
@@ -28,6 +29,28 @@ class Gammma
     @input_val = input_val_arg
     @quantity = quantity_arg
     @year_to_date = year_to_date
+  end
+
+  def compute
+    @inportant_value1 = (input_val * quantity) + @account.delta
+    @important_value2 = (input_val * year_to_date) + 100
+    important_thing
+    @important_value3 = important_value2 * 7
+    @important_value3 -2 * important_value1
+  end
+
+  # 引数渡しなどの心配をせずにcomputeメソッドに対してメソッドの抽出をできる
+  def important_thing
+    if(year_to_date -important_value1) > 100
+      @important_value2 -= 20
+    end
+  end
+end
+
+# 
+class Account
+  def gamma(input_val, quantity, year_to_date)
+    Gammma.new(self, input_val, quantity, year_to_date).compute
   end
 end
 
