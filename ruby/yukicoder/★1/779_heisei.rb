a27860d9efa9edb3e2254d39bae01ab6ed37bb63
hogucc <uku.h1r8@gmail.com>
2022-02-05 11:05:52 +0900

    add ruby/yukicoder/★1/779_heisei.rb

---
+++ ruby/yukicoder/★1/779_heisei.rb
@@ -0,0 +1,15 @@
+# 1990から2018の間なら平成確定
+# 1989年なら1月7以前は平成じゃない
+# 2019年5月以降は平成じゃない
+ymd = gets.chomp.split.map(&:to_i)
+
+case ymd
+in [1989, 1, ..7]
+  puts 'No'
+in [2019, 5.., *]
+  puts 'No'
+in [1989..2019, *]
+  puts 'Yes'
+else
+  puts 'No'
+end
